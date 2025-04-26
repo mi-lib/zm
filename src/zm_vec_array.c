49b05f704653cdb7f544685e454d72e9385482b3
kdaic <develop@kdaic.net>
2025-04-26 19:34:02 +0900

    Add zNURBS Fitting API, zm_nurbs_fitting, nurbs_fitting_sample, and Add zVecCreateFromzVecArray, zVecArrayCreateFromzVec into zm_vec_array

--- src/zm_vec_array.c
+++ src/zm_vec_array.c
@@ -45,3 +45,32 @@ zVec zVecArrayFill(zVecArray *array, zVec v)
     zVecCopyNC( v, *zArrayElem(array,i) );
   return v;
 }
+
+/* create zVec from a zVecArray */
+zVec *zVecCreateFromzVecArray(const zVecArray *src_array, zVec *dest_vec)
+{
+  int i, vec_size, array_size;
+
+  vec_size = zVecSize( *zArrayElemNC(src_array, 0) );
+  array_size = zArraySize(src_array);
+  *dest_vec = zVecAlloc( vec_size * array_size );
+  for( i=0; i<array_size; i++ )
+    zVecPutNC( *dest_vec, i * vec_size, *zArrayElemNC( src_array, i ) );
+  return dest_vec;
+}
+
+/* create zVecArray from a zVec */
+zVecArray *zVecArrayCreateFromzVec(const zVec src_vec, const int vec_size, const int array_size, zVecArray *dest_array)
+{
+  int i;
+
+  if( vec_size * array_size != zVecSize(src_vec) ){
+    ZRUNERROR( ZM_ERR_VEC_SIZEMISMATCH );
+    return NULL;
+  }
+  zVecArrayAlloc( dest_array, vec_size, array_size );
+  for( i=0; i<array_size; i++ )
+    zVecGetNC( src_vec, vec_size * i, *zArrayElemNC(dest_array, i) );
+  return dest_array;
+}
+
